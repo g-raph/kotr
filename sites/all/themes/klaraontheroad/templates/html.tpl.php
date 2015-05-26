86a15a38dc10a09350aba336e0da82fcd5ab983f
G-raph <info@g-raph.be>
2015-05-26 10:12:44 +0200

    Fontawesome library + calendar view.

--- sites/all/themes/klaraontheroad/templates/html.tpl.php
+++ sites/all/themes/klaraontheroad/templates/html.tpl.php
@@ -51,6 +51,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
   <link rel="stylesheet" href="/sites/all/themes/klaraontheroad/css/style.css" />
   <link rel="stylesheet" href="/sites/all/themes/klaraontheroad/css/style-desktop.css" />
   <link rel="stylesheet" href="/sites/all/themes/klaraontheroad/css/timeline.css" />
