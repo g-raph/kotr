25717cfe4be70a790cc94e1f7e15c283900d3107
G-raph <info@g-raph.be>
2015-06-09 14:39:39 +0200

    No slideshow.

--- logo/klara.js
+++ logo/klara.js
@@ -2,9 +2,9 @@ jQuery(document).ready(function($){
 	$('body').flexslider({
         selector: ".presentation > div",
         animation: "fade",
-        slideshowSpeed: 4000,
         controlNav: false,
         directionNav: true,
-        pausePlay: false
+        pausePlay: false,
+        slideshow: false
     });
 });
