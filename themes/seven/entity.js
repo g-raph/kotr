ea58ab18ead5e443efaf0f55081b7f6429a0c0a7
G-raph <info@g-raph.be>
2015-06-11 14:46:04 +0200

    Fix for double entry title/name for entities.

--- themes/seven/entity.js
+++ themes/seven/entity.js
@@ -2,12 +2,20 @@
     Drupal.behaviors.entity = {
         attach: function (context, settings) {
 
-            // entity song double title fix
-            var songTitleFieldOrig = $('.page-admin-structure-entity-type-song-song #eck-entity-form-add-song-song .form-item-title input');
-            var songTitleFieldFake = $('.page-admin-structure-entity-type-song-song #eck-entity-form-add-song-song .form-item-field-song-title-und-0-value input');
-            songTitleFieldOrig.hide();
-            songTitleFieldFake.change(function() {
-                songTitleFieldOrig.val($(this).val());
+            // entity componist double title fix
+            var compTitleFieldOrig = $('#eck-entity-form-add-componist-componist .form-item-title input');
+            var compTitleFieldFake = $('#eck-entity-form-add-componist-componist  .field-name-field-componist-name input');
+            $('#eck-entity-form-add-componist-componist .form-item-title').hide();
+            compTitleFieldFake.change(function() {
+                compTitleFieldOrig.val($(this).val());
+            });
+
+            // entity uitvoerder double title fix
+            var uitvTitleFieldOrig = $('#eck-entity-form-add-uitvoerder-uitvoerder .form-item-title input');
+            var uitvTitleFieldFake = $('#eck-entity-form-add-uitvoerder-uitvoerder  .field-name-field-uitv-name input');
+            $('#eck-entity-form-add-uitvoerder-uitvoerder .form-item-title').hide();
+            uitvTitleFieldFake.change(function() {
+                uitvTitleFieldOrig.val($(this).val());
             });
 
         }
